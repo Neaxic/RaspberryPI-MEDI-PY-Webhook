68827ead7f9b11c7c52c0597a344e2dfab0f1406
Andre <andreas79001@gmail.com>
2024-09-03 17:51:21 +0200

    hardcode subnet now

--- MasterPI/app.py
+++ MasterPI/app.py
@@ -21,8 +21,21 @@ pc = 0
 # Create a lock for synchronizing access to the msg variable
 msg_lock = threading.Lock()
 
-# Define the subnet
-SUBNET = '192.168.1.'
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+# Dynamically get the IP address
+IP_ADDRESS = get_ip_address()
+print(f"Server IP: {IP_ADDRESS}")
 
 @app.route('/master', methods=['GET'])
 def webhook():
--- SlavePI/app.py
+++ SlavePI/app.py
@@ -1,25 +1,9 @@
 import requests
-import subprocess
 import time
 import socket
 
-# URL of the localhost API
 PORT = 2000
-SUBNET = '192.168.1.'
-getinterval = 10 #in secounds
-api_endpoint = "master"  # Fixed string formatting for port
-
-# Variable to store the last variable name
-last_variable_name = None
-
-# Dictionary mapping variable names to file names
-file_dict = {
-    'var1': 'file1.png',
-    'var2': 'file2.png',
-    'var3': 'file3.png',
-    'var4': 'file4.png',
-    'var5': 'file5.png',
-}
+SUBNET = '192.168.86.'  # Update this to match your actual subnet
 
 def find_master():
     for i in range(1, 255):  # Scan all possible IP addresses in the subnet
